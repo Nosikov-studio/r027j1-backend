a9ff4b3c0f532ca2a08e5daca90401006f2dbe64
Nos <zvezdazanatih@gmail.com>
2025-05-27 10:22:55 +0700

    0

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const login = require('./routes/login');
+const test = require('./routes/test');
+const logout = require('./routes/logout');
+
+const app=express();
+
+app.use(express.json());
+
+app.post('/login', login);
+app.post('/test', test);
+app.post('/logout', logout);
+
+app.listen(50555);
---
+++ routes/login.js
@@ -0,0 +1,20 @@
+const {v4: uuid} = require("uuid");
+const sessions = require('../sessions');
+
+module.exports = async (req, res) => {
+    const {username, password} = req.body;
+
+    if (username !== "admin" || password !== "admin") {
+        return res.status(403).send("authentication failed")
+    }
+
+    const sessionId = uuid();
+    sessions[sessionId]= { username, userID: 1};
+
+    console.log(sessions);
+
+    res.cookie("session", sessionId, {
+        httpOnly: true,
+    })
+    res.send("Success");
+}
