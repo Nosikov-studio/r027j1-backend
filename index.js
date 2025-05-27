4115c83f0a1a4afa0e9b83740be6a0558613841c
Nos <zvezdazanatih@gmail.com>
2025-05-27 11:45:44 +0700

    9

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const app=express();
 app.use(express.json());
 
 app.post('/login', login);
-app.post('/test', test);
-app.post('/logout', logout);
+//app.post('/test', test);
+//app.post('/logout', logout);
 
 app.listen(40444, () => {
         console.log('Сервер запущен на порту 40444')});
