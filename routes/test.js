46ceab4566ca285928f97c9462386c24552db0cb
Nos <zvezdazanatih@gmail.com>
2025-05-27 17:13:34 +0700

    23

--- routes/test.js
+++ routes/test.js
@@ -1,7 +1,17 @@
 const sessions = require('../sessions');
 
 module.exports = async (req, res) => {
-    const sessionId = req.headers.cookie?.split("=")[1];
+
+// улучшенная обработка кук
+const cookies = req.headers.cookie?.split(';').reduce((acc, cookie) => {
+const [name, value] = cookie.trim().split('=');
+acc[name] = value;
+return acc;
+}, {});
+const sessionId = cookies?.sessionId;   
+    
+    
+    // const sessionId = req.headers.cookie?.split("=")[1];
     const userSession = sessions[sessionId];
 
     if (!userSession) {
